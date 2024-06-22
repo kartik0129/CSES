95c1a92e24812d01332e377343092b38791d65cf
kartik0129 <cooldude.kartik.123@gmail.com>
2024-06-22 22:33:20 +0530

    day41

---
+++ Round_Trip.cpp
@@ -0,0 +1,81 @@
+// define KPR_111
+
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+int n, m;
+int sv, ev;
+vector<vector<int>> adj;
+vector<int> vis;
+vector<int> parent;
+ 
+ 
+bool dfs(int u, int p)
+{
+	vis[u] = true;
+	parent[u] = p;
+	for(auto v: adj[u])
+	{
+		if(v == p) continue;
+		if(vis[v]) 
+		{
+			sv = v; 
+			ev = u; 
+			return true;
+		} 
+		if(!vis[v]) 
+			if(dfs(v,u)) 
+				return true;
+	}
+	return false;
+}
+ 
+ 
+bool visit_all()
+{
+	for(int i = 1; i <= n; ++i)
+	{
+		if(!vis[i])
+			if(dfs(i,-1)) return true;
+	}
+	return false;
+}
+ 
+int32_t main()
+{
+	cin >> n >> m;
+	adj.resize(n+1);
+	vis.resize(n+1);
+	parent.resize(n+1);
+	for(int i =0 ; i < m; ++i)
+	{
+		int u, v;
+		cin >> u >> v;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+ 
+	if(!visit_all())
+	{
+		cout << "IMPOSSIBLE" << endl;
+		return 0;
+	}
+ 
+	int tv = ev;
+	vector<int> ans;
+	ans.push_back(ev);
+	while(tv != sv)
+	{
+		ans.push_back(parent[tv]);
+		tv = parent[tv];
+	}
+	ans.push_back(ev);
+	cout << ans.size() << endl;
+	for(auto c: ans)
+	{
+		cout << c << " ";
+	}
+ 
+}
